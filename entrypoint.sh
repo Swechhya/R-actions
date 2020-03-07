d4b9168be61f155fcf1b145f5c748c73cf755543
Swechhya <swechhyabista@lftechnology.com>
2020-03-07 18:50:43 +0545

    Add additional dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 echo "Installing R and dependencies"
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y r-base
-# apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
-# apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
-# apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
-# apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr 
-# apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql
+apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
+apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
+apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
+apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr 
+apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql
 
 
 # Check if description file exits
@@ -28,10 +28,12 @@ fi
 
 if [ "$1" = "test" ]; then
     echo "Running only test task"
+    R CMD check ./
     
 fi
 
 if [ "$1" = "all" ]; then
     echo "Running all tasks"
     R CMD build ./
+    R CMD check ./
 fi
