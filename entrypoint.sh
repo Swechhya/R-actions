ac33d1c7c749499621dff456ea37132db9c74a6f
Swechhya <swechhyabista@lftechnology.com>
2020-03-07 14:42:23 +0545

    Remove sudo as we are running in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Install R
 echo "Installing R and dependencies"
-sudo apt-get update
-sudo apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
-sudo apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
-sudo apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr 
-sudo apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql
+apt-get update
+apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
+apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
+apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
+apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr 
+apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql
 
 
 # Check if description file exits
