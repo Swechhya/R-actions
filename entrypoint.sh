e41691ec90dd4dbd8b969b5ea8e96b416ccb94a9
Swechhya <swechhyabista@gmail.com>
2022-10-16 06:35:52 +0545

    Use r-base

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ cd $3
 # Install R
 echo "\e[1mInstalling R and dependencies"
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y r-base
 apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
 apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
 apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev 
